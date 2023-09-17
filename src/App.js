020e9f2df89387d7e3584c8ae92b5ae973725a54
sedrick Gerard <sedrickgerard@gmail.com>
2023-09-17 05:18:55 +0100

    finished variable esporting

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@ import "./App.css";
 function App() {
 const title = "Welcome to this REact Ninja-Blog"
 const likes = 50;
-
+const googleLink = "http://www.google.com";
+const myArray = [1, 2, 3, 4, 5];
   return (
     <div className="App">
       <div className="content">
         <h1> { title }</h1>
         <p>{ likes } times</p>
+
+        <p> { "Hello folks"}</p>
+        <p> { [myArray]}</p>
+        <p> {[5,4,3,2,1]}</p>
+
+        <a href={googleLink}>  google site </a>
       </div>
     </div>
   );
