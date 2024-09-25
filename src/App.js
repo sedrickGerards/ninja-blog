45f17694a543c8f5c14f7089f92915fe7e42359f
sedrick Gerard <sedrickgerard@gmail.com>
2024-09-25 20:18:38 +0100

    setting up todo list with react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import Todo from "./Components/Todo";
 function App() {
-
   return (
-    <div>✋ Hello! React World</div>
+    <div>
+      <h1> My Todos</h1>
+     <Todo text='Pray'/>
+     <Todo text='Meditate' />
+     <Todo text='Preview messages'/>
+     <Todo text='Get out'/>
+    </div>
   );
-
 }
 
 export default App;
